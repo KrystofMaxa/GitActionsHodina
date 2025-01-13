53db4373e9c88865c7fe9efa26aa887b95155f3b
Pepa z Depa <krystof.maxa@student.gyarab.cz>
2025-01-13 09:58:48 +0100

    first commit

---
+++ src/main/java/org/example/Kalkulator.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class Kalkulator {
+    int x;
+    int y;
+
+    public Kalkulator(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+    public int nasobic(){
+        return x*y;
+    }
+    public int scitanec(){
+        return x+y;
+    }
+}
---
+++ src/test/java/org/example/KalkulatorTest.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class KalkulatorTest {
+
+    @org.junit.jupiter.api.Test
+    void nasobic() {
+        Kalkulator kalkulator = new Kalkulator(3,4);
+        assertTrue(kalkulator.nasobic()==12);
+    }
+
+    @org.junit.jupiter.api.Test
+    void scitanec() {
+        Kalkulator kalkulator = new Kalkulator(3,4);
+        assertTrue(kalkulator.scitanec()==7);
+    }
+}
