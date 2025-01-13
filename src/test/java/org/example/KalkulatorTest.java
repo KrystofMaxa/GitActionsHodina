6ca523a5896072cf20f65245bae9e2eeae1211b7
Pepa z Depa <krystof.maxa@student.gyarab.cz>
2025-01-13 11:17:30 +0100

    Oprava Testu

--- src/test/java/org/example/KalkulatorTest.java
+++ src/test/java/org/example/KalkulatorTest.java
@@ -1,16 +1,18 @@
 package org.example;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class KalkulatorTest {
 
-    @org.junit.jupiter.api.Test
+    @Test
     void nasobic() {
         Kalkulator kalkulator = new Kalkulator(3,4);
         assertTrue(kalkulator.nasobic()==12);
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void scitanec() {
         Kalkulator kalkulator = new Kalkulator(13,4);
         assertTrue(kalkulator.scitanec()==7);
